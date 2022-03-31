92fbe7b2f365d702329867c5c7f768494543b13b
dimsunv <dimsunv@gmail.com>
2022-04-01 00:25:35 +0300

    done*

--- Practic/test.py
+++ Practic/test.py
@@ -3,7 +3,14 @@ from os import path
 import json
 import yaml
 
-
 file = sys.argv[1]
 full_name = path.splitext(path.basename(file))
-print(full_name)
+
+if full_name[1] in '.json':
+    print('Это json')
+elif full_name[1] in ('.yml', '.yaml'):
+    print('Это yaml')
+else:
+    print('Incorrect file format')
+    exit(0)
+
